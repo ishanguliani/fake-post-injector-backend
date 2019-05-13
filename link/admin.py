f288c268c3fa0aa753772100f28ccf17874136be
Ishan Guliani <ishan.guliani93@gmail.com>
2019-05-13 14:08:25 -0400

    added new property to display parent link in the list_display field of link admin class

--- link/admin.py
+++ link/admin.py
@@ -4,7 +4,7 @@ from link.models import LinkModel, LinkType
 # Register your models here.
 @admin.register(LinkModel)
 class LinkAdmin(admin.ModelAdmin):
-    list_display = ['link_text_original', 'link_text_fake', 'link_target_original', 'link_target_fake', 'link_type', 'authored_text_original', 'authored_text_fake', 'author_name', 'is_seen', 'is_clicked', 'user']
+    list_display = ['link_text_original', 'link_text_fake', 'property_link_target_original', 'property_link_target_fake', 'link_image_src_original', 'link_type', 'property_authored_text_original', 'authored_text_fake', 'author_name', 'is_seen', 'is_clicked', 'user']
 
 @admin.register(LinkType)
 class LinkTypeAdmin(admin.ModelAdmin):
