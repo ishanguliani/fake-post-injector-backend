beacf9ccf32a88d9f3a958e81fa10b72129b525b
Ishan Guliani <ishan.guliani93@gmail.com>
2019-05-13 14:11:22 -0400

    Updated link/models/py with the following -
    1. redefined the meaning of linkType field - 4 means injected fake link, 3 means link with changed reactions and 1 means genuine untouched link
    2. added support to save the img src link of the original post as the field link_image_src_original
    3. added some properties to the link/models.py LinkModel class to display the fields in the admin panel with truncated text

--- link/models.py
+++ link/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from user.models import User
-
+from django.template.defaultfilters import truncatechars
 class LinkType(models.Model):
     """
     What type will a link be ?
         1. genuine
-        2. fake
-        3. modified_genuine
+        4. injected
+        3. reactions_changed
     """
     type = models.CharField(max_length=30, blank = False, default = "genuine")
 
@@ -19,6 +19,7 @@ class LinkModel(models.Model):
     link_text_fake = models.CharField(max_length=1000, blank=True)
     link_target_original = models.CharField(max_length=1000, blank=True)
     link_target_fake = models.CharField(max_length=1000, blank=True)
+    link_image_src_original = models.CharField(max_length=1000, blank=True)
     link_type = models.ForeignKey(LinkType, on_delete=models.SET_NULL, null=True)
     authored_text_original = models.CharField(max_length=1000, blank=True)
     authored_text_fake = models.CharField(max_length=1000, blank=True)
@@ -27,6 +28,26 @@ class LinkModel(models.Model):
     is_clicked = models.BooleanField(default=False)
     time_to_view = models.TimeField(blank = True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    preview_title = models.CharField(max_length=1000, blank=True, default='')
+    preview_description = models.CharField(max_length=2000, blank=True, default='')
+    preview_image = models.CharField(max_length=1000, blank=True, default='')
+    preview_url = models.CharField(max_length=1000, blank=True, default='')
+
+    @property
+    def property_link_target_original(self):
+        return truncatechars(self.link_target_original, 50)
+
+    @property
+    def property_link_target_fake(self):
+        return truncatechars(self.link_target_fake, 100)
+
+    @property
+    def property_authored_text_original(self):
+        return truncatechars(self.authored_text_original, 100)
+
+    @property
+    def property_link_image_src_original(self):
+        return truncatechars(self.link_image_src_original, 1000)
 
     def __str__(self):
         return str(self.link_text_original[:25]) + '..' + ' by ' + str(self.user)
