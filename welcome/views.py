06cfc54547fc2f8da1bcdd7bfed48833f40aeeb4
Ishan Guliani <ishan.guliani93@gmail.com>
2019-05-27 17:45:42 -0400

    added Date to welcome page

--- welcome/views.py
+++ welcome/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from user.forms import UserModelForm
-
+import datetime
 # Create your views here.
 
 def showWelcomePage(request):
-    return render(request, "welcome/welcome.html", {'form' : UserModelForm()})
+    d = datetime.datetime.today()
+    return render(request, "welcome/welcome.html", {'form' : UserModelForm(), 'date': str(d.month) + "-" + str(d.day) + "-" + str(d.year)})
 
 def signupSuccess(request, id):
     return render(request, "welcome/signup_success.html", {'id' : id})
