3c512a0a05d2a2f1e7f0b9ecc687750bfd764c80
Ishan Guliani <ishan.guliani93@gmail.com>
2019-05-27 17:54:20 -0400

    Added feature to upload fake links from the backend

--- fakeLinkModel/admin.py
+++ fakeLinkModel/admin.py
@@ -3,4 +3,4 @@ from .models import FakeLinkModel
 # Register your models here.
 @admin.register(FakeLinkModel)
 class FakeLinkModelAdmin(admin.ModelAdmin):
-    list_display = ['fake_picture', 'fake_link_text', 'fake_link_description', 'fake_link', 'fake_link_header']
+    list_display = ['id', 'fake_picture', 'fake_link_text', 'fake_link_description', 'fake_link', 'fake_link_header']
