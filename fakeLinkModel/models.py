6a5d263df19ea5b1d178dbc2a84284a60e8ae896
Ishan Guliani <ishan.guliani93@gmail.com>
2019-05-28 18:08:15 -0400

    added new field fake_link_topic to fakeLinkModel

--- fakeLinkModel/models.py
+++ fakeLinkModel/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class FakeLinkModel(models.Model):
     fake_picture = models.ImageField(upload_to='images/', verbose_name='Fake Picture')
+    fake_link_topic = models.CharField(max_length=1000, default='Entertainment', blank=True)
     fake_link_text = models.CharField(max_length=1000, default='', blank=True)
     fake_link_description = models.CharField(max_length=1000, default='', blank=True)
     fake_link = models.CharField(max_length=1000, default='', blank=True)
