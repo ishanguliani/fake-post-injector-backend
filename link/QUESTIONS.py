26371591405c5d4d14b74a6ccbf3c7481b4ab064
ishanguliani <ishan.guliani93@gmail.com>
2020-02-06 18:07:12 -0500

    [updated survey] new questions added to the file QUESTIONS.py

--- link/QUESTIONS.py
+++ link/QUESTIONS.py
@@ -1,1 +1,12 @@
-questions = ['What is the topic of the post?', 'What is your relationship with the author?',  'Why do you find this post interesting?', 'Did you click on this post?']
+# old questions
+# questions = ['What is the topic of the post?', 'What is your relationship with the author?',  'Why do you find this post interesting?', 'Did you click on this post?']
+# new questions
+questions = ['Did you click on this link?' # Question 1
+    , 'Does the author usually share posts like this?' # Question 2
+    , 'Why did you click or not click on this link?' # Question 3
+    , 'How do you describe your relationship with this person?'  # Question 4
+    , 'What is the topic of the post?' # Question 5
+    , 'How frequently do you notice this person sharing a post similar to this?' # Question 6
+    , 'What types of posts does this person generally share? [For this question, participant should be able to choose multiple options]' # Question 7
+    , 'How frequently do you click on a post shared by this person?' # Question 8
+    ]
