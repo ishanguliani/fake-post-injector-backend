1f7bd6cd7f3fe2cdf46ef2ff6f4af63cb6ca4adf
ishanguliani <ishan.guliani93@gmail.com>
2020-04-16 21:41:05 -0400

    minor fix

--- report/models.py
+++ report/models.py
@@ -3,11 +3,11 @@ from user.models import User
 from link.models import LinkModel
 
 # Create your models here.
-class BriefSummary(models.model):
+class BriefSummary(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     numberOfLinksSeen = models.IntegerField(default=0, null=True)
     numberOfLinksClicked = models.IntegerField(default=0, null=True)
 
-class DetailedSummary(models.model):
+class DetailedSummary(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     linkModel = models.ForeignKey(LinkModel, on_delete=models.SET_NULL)
