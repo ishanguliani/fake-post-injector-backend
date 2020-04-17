4df62fcd5bd19b948b7ee56e33e724f0c4a8ea25
ishanguliani <ishan.guliani93@gmail.com>
2020-04-16 22:58:34 -0400

    move short link generation logic to the time when data is requested

--- fakeLinkModel/models.py
+++ fakeLinkModel/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from configuration.views import convertLongLinkToShortLink
+
 # Create your models here.
 class FakeLinkModel(models.Model):
     fake_picture = models.ImageField(upload_to='images/', verbose_name='Fake Picture')
@@ -9,17 +9,6 @@ class FakeLinkModel(models.Model):
     fake_link = models.CharField(max_length=1000, default='', blank=True)
     fake_link_header = models.CharField(max_length=1000, default='', blank=True, verbose_name='Fake link source website')
     short_link = models.CharField(max_length=1000, default='', blank=True)
-
-    def __init__(self, fake_picture, fake_link_topic, fake_link_text, fake_link_description, fake_link,
-                 fake_link_header, short_link, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fake_picture = fake_picture
-        self.fake_link_topic = fake_link_topic
-        self.fake_link_text = fake_link_text
-        self.fake_link_description = fake_link_description
-        self.fake_link = fake_link
-        self.fake_link_header = fake_link_header
-        self.short_link = "https://seng-research.com/track/" + convertLongLinkToShortLink(self.fake_link)
 
     def __str__(self):
         return "FakeLinkModel{" + 'fake_link_topic' + str(self.fake_link_topic) + 'fake_link_text' + str(self.fake_link_text)[:20] + \
