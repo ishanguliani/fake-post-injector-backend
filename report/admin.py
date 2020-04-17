85bff88365df2a5ac67bb150b654ed9066f8d0a8
ishanguliani <ishan.guliani93@gmail.com>
2020-04-16 21:57:12 -0400

    added model admin for DetailedSummary

--- report/admin.py
+++ report/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from import_export import resources
-from report.models import BriefSummary
+from report.models import BriefSummary, DetailedSummary
 
 class BriefSummaryResource(resources.ModelResource):
     class Meta:
@@ -14,4 +14,16 @@ class BriefSummaryModelAdmin(ImportExportModelAdmin):
     resource_class =  BriefSummaryResource
     list_display = ['user', 'numberOfLinksSeen', 'numberOfLinksClicked']
 
+class DetailedSummaryResource(resources.ModelResource):
+    class Meta:
+        model = DetailedSummary
+        field = ('user', 'linkModel')
+        export_order = ('user', 'linkModel')
+
+# Register your models here.
+class DetailedSummaryModelAdmin(ImportExportModelAdmin):
+    resource_class =  DetailedSummaryResource
+    list_display = ('user', 'linkModel')
+
 admin.site.register(BriefSummary, BriefSummaryModelAdmin)
+admin.site.register(DetailedSummary, DetailedSummaryModelAdmin)
