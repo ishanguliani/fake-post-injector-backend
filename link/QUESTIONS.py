df185ac1f73182fed98ebd3837f93c188b00d5c8
ishanguliani <ishan.guliani93@gmail.com>
2020-04-17 20:30:09 -0400

    [bugfix]: where question 10 answers in the survey were not getting recorded
    - this occurred due to a change made on Apr 2nd in QUESTIONS.py where a string was changed in one place but not changed in another.

--- link/QUESTIONS.py
+++ link/QUESTIONS.py
@@ -3,15 +3,18 @@
 CHOICE_TEXT_OTHER = 'Other'
 ADDITIONAL_INSTRUCTIONS_FOR_MULTIPLE_CHOICE_QUESTIONS = "\nNOTE: Please enter your answers separated by commas. For `" + CHOICE_TEXT_OTHER + "` option, add your response at the end.\nFor example: If your answer is option a, option c and " + CHOICE_TEXT_OTHER + " then please write: a,c,this my answer to this question"
 # new questions
+WHY_DID_YOU_CLICK = "Why did you click on this link?"
+WHY_DID_YOU_NOT_CLICK = "Why did you not click on this link?"
+WHAT_TYPES_OF_POSTS = "What types of posts does this account generally share?"
 QUESTIONS = {0: 'Did you click on this link?'  # Question 1
     , 1: 'Does this account usually share posts like this?'  # Question 2
-    , 21: "Why did you click on this link? Please select all that apply. \n "
+    , 21: WHY_DID_YOU_CLICK + ' ' + "Please select all that apply. \n "
           "a. The content/preview looks interesting \n "
           "b. I would like to discuss with the post author about the content in the link at a later time \n "
           "c. The author doesn't usually share this kind of content in social network \n "
           "d. " + CHOICE_TEXT_OTHER + ", please specify\n "
           "" + ADDITIONAL_INSTRUCTIONS_FOR_MULTIPLE_CHOICE_QUESTIONS  # Question 31 - Others
-    , 22: "Why did you NOT click on this link? Please select all that apply. \n "
+    , 22: WHY_DID_YOU_NOT_CLICK + ' ' + "Please select all that apply. \n "
           "a. The author doesn't usually share this kind of content in social network \n "
           "b. The content/preview of the link is not relevant to my interest \n "
           "c. I do not think the shared link is authentic \n "
@@ -22,7 +25,7 @@ QUESTIONS = {0: 'Did you click on this link?'  # Question 1
     , 4: 'What is the topic of the post?'  # Question 5
     , 41: "Specify topic below if you selected 'Others' above"  # Question 51 - Others
     , 5: 'How frequently do you notice this account sharing a post similar to this?'  # Question 6
-    , 6: 'What types of posts does this account generally share? Select all that apply.\n '
+    , 6: WHAT_TYPES_OF_POSTS + ' ' + 'Select all that apply.\n '
          'a. Sales-oriented (e.g. clothes sales, modeling opportunities)\n '
          'b. Media (e.g. videos, pictures)\n '
          'c. Interactives (e.g. quizzes, games)\n '
@@ -31,7 +34,7 @@ QUESTIONS = {0: 'Did you click on this link?'  # Question 1
          "f. " + CHOICE_TEXT_OTHER + "\n "
          '' + ADDITIONAL_INSTRUCTIONS_FOR_MULTIPLE_CHOICE_QUESTIONS  # Question 7
     , 7: 'How frequently do you click on a post shared by this account?'  # Question 8
-             }
+            }
 
 # the following questions will be exclude from choice extraction process
 # since they have the "Others" option in them
@@ -42,9 +45,9 @@ OTHER_OPTION_CHOICE_QUESTION_SET.add(41)
 # the following questions are of input type and hence will have their choice
 # directly extracted from the incoming POST request
 INPUT_TEXT_MULTIPLE_CHOICE_QUESTION_SET = set()
-INPUT_TEXT_MULTIPLE_CHOICE_QUESTION_SET.add((21, "Why did you click on this link?"))
-INPUT_TEXT_MULTIPLE_CHOICE_QUESTION_SET.add((22, "Why did you not click on this link?"))
-INPUT_TEXT_MULTIPLE_CHOICE_QUESTION_SET.add((6, "What types of posts does this person generally share"))
+INPUT_TEXT_MULTIPLE_CHOICE_QUESTION_SET.add((21, WHY_DID_YOU_CLICK))
+INPUT_TEXT_MULTIPLE_CHOICE_QUESTION_SET.add((22, WHY_DID_YOU_NOT_CLICK))
+INPUT_TEXT_MULTIPLE_CHOICE_QUESTION_SET.add((6, WHAT_TYPES_OF_POSTS))
 
 choicesByQuestion = {
     0:['Yes (if you choose this option, skip question #4)',
