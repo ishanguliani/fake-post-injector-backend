780dcffc9d8a06bf5ab8811a67e1e656a23ee850
ishanguliani <ishan.guliani93@gmail.com>
2020-05-04 18:47:42 -0400

    update link seen count in reports
    - whenever a user encounters a new link

--- report/views.py
+++ report/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from .models import BriefSummary
 
 # Create your views here.
-def trackLink(request, short_link):
+def trackLink(request, userid, short_link):
     """
     count this link click and route the user to the appropriate link
     """
     if request.method == 'GET':
         pass
+
+def updateReportLinkSeenIncrement(user):
+    """
+    invoked whenever there is an update to the report to
+    increment the link seen count for the given user
+    """
+    existingEntry = BriefSummary.objects.filter(user = user)
+    if not existingEntry:
+        # no report entry found for this user, lets create a new report entry
+        existingEntry = BriefSummary(user = user)
+        print("updateReportLinkSeenIncrement: new entry created!")
+    print("updateReportLinkSeenIncrement: report entry: ", str(existingEntry))
+    existingEntry.numberOfLinkSeen += 1
+    existingEntry.save()
