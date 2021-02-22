3d87d8886d6e6c9f7ed0cfbfa7f5a76f1b841714
ishanguliani <ishan.guliani93@gmail.com>
2021-02-22 23:11:15 +0530

    updated default value for alias and mturk_id to be empty

--- user/models.py
+++ user/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class User(models.Model):
 
     name = models.CharField(max_length=50, blank=True)
-    alias = models.CharField(max_length=80, blank=False, default='NOT_SET')
+    alias = models.CharField(max_length=80, blank=False, default='')
     email = models.EmailField(blank=True)
-    mturk_id = models.CharField(max_length=100, blank=False, default='NOT_SET')
+    mturk_id = models.CharField(max_length=100, blank=False, default='')
     attendance_id = models.CharField(max_length=50, blank=True, null=True)
     uuid = models.CharField(max_length=100, blank=True, null=True)
 
