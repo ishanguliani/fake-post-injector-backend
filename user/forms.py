88d6095cf933a391c39ceccb359d936d25c5115e
ishanguliani <ishan.guliani93@gmail.com>
2021-02-22 22:57:53 +0530

    stopped accepting pii from user during registration

--- user/forms.py
+++ user/forms.py
@@ -4,16 +4,16 @@ from .models import User
 class UserModelForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['name', 'email', 'attendance_id']
+        fields = ['alias', 'mturk_id', 'attendance_id']
 
         labels = {
-            'name': 'What do we call you? (Name)',
-            'email': 'How do we reach you? (Email id)',
+            'alias': 'Enter your alias or username',
+            'mturk_id': 'Enter your mturk id',
             'attendance_id': 'Enter the attendance id provided to you today',
         }
         help_texts = {
-            'name': 'Type your name',
-            'email': 'Type your email',
+            'alias': 'Type your alias',
+            'mturk_id': 'Type your mturk id',
             # 'attendance_id': 'Attendance id provided by the survey incharge',
         }
 
--- user/models.py
+++ user/models.py
@@ -3,16 +3,18 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
 
-    name = models.CharField(max_length=50, blank=False)
-    email = models.EmailField(blank=False)
+    name = models.CharField(max_length=50, blank=True)
+    alias = models.CharField(max_length=80, blank=False)
+    email = models.EmailField(blank=True)
+    mturk_id = models.CharField(max_length=100, blank=False)
     attendance_id = models.CharField(max_length=50, blank=True, null=True)
     uuid = models.CharField(max_length=100, blank=True, null=True)
 
     def setUuid(self):
-        newUuid = str(abs(hash(str(self.name) + str(self.email) + str(self.attendance_id)))%100000000)
+        newUuid = str(abs(hash(str(self.name) + str(self.alias) + str(self.email) + str(self.mturk_id) + str(self.attendance_id)))%100000000)
         self.uuid = newUuid
         print('changed success!', str(self.uuid))
         self.save()
 
     def __str__(self):
-        return  "User{ " + 'id: ' + str(self.id) + ', name: ' + str(self.name) + ', email: ' + str(self.email) + ", uuid: " + str(self.uuid)[:10] + " }"
+        return  "User{ " + 'id: ' + str(self.id) + ', name: ' + str(self.name) + ', alias: ' + str(self.alias) + ', email: ' + str(self.email) + ', mturk id: ' + str(self.mturk_id) + ", uuid: " + str(self.uuid)[:10] + " }"
