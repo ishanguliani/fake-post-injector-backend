848f62c95da783d9b111ec78d003c2241e95ad77
ishanguliani <ishan.guliani93@gmail.com>
2021-02-27 23:50:42 +0530

    added shown_date_and_time to linkModel admin

--- link/admin.py
+++ link/admin.py
@@ -19,7 +19,7 @@ class LinkModelResource(resources.ModelResource):
 
 class LinkModelAdmin(ImportExportModelAdmin):
     resource_class = LinkModelResource
-    list_display = ['link_type',  'link_text_original', 'link_text_fake', 'is_clicked', 'is_clicked_event_from_ground_data', 'is_clicked_event_from_ground_data_time', 'user', 'property_link_target_original', 'property_link_target_fake', 'link_image_src_original', 'property_authored_text_original', 'author_name', 'is_seen']
+    list_display = ['link_type',  'link_text_original', 'link_text_fake', 'is_clicked', 'is_clicked_event_from_ground_data', 'is_clicked_event_from_ground_data_time', 'shown_date_and_time',  'user', 'property_link_target_original', 'property_link_target_fake', 'link_image_src_original', 'property_authored_text_original', 'author_name', 'is_seen']
     readonly_fields = ('id', 'link_text_original', 'link_text_fake', 'link_target_original', 'link_target_fake', 'link_image_src_original', 'link_type', 'authored_text_original', 'authored_text_fake', 'author_name', 'is_seen', 'is_clicked', 'is_clicked_event_from_ground_data', 'is_clicked_event_from_ground_data_time', 'preview_title', 'preview_description', 'preview_image', 'preview_url', 'shown_date_and_time', 'user')
 
 # @admin.register(LinkType)
